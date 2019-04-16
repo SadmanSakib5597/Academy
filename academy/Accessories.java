78f50712da06ad023855b44fd3408cb990b31026
SadmanSakib5597 <46521025+SadmanSakib5597@users.noreply.github.com>
2019-04-15 23:45:09 -0700

    CHANGE
    NEW UPLOAD

---
+++ academy/Accessories.java
@@ -0,0 +1,28 @@
+package academy;
+/*
+public class Accessories {
+
+    private String acctyp;
+    public String accNum;
+    private double balance;
+
+    public Bank (String accNum, String acctyp, double balance) {
+
+        this.accNum = accNum;
+        this.acctyp = acctyp;
+        this.balance = balance;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ("Account type: " + this.acctyp+'\n');
+    }
+
+}
+*/
---
+++ academy/Newinput.java
@@ -0,0 +1,26 @@
+package academy;
+
+public class Newinput {
+   /* private String acctyp;
+    public String accNum;
+    private double balance;
+
+    public Bank (String accNum, String acctyp, double balance) {
+
+        this.accNum = accNum;
+        this.acctyp = acctyp;
+        this.balance = balance;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ("Account type: " + this.acctyp+'\n');
+    }
+*/
+}
